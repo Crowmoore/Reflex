404d434d76a5f69f4df3eda0da3f8358b14a3bcd
Crowmoore <jokke.kortelainen@gmail.com>
2016-10-02 18:12:05 +0300

    Fix a bug that causes game to end when volume button is pressed

--- app/src/main/java/fi/crowmoore/reflextester/RegularPlay.java
+++ app/src/main/java/fi/crowmoore/reflextester/RegularPlay.java
@@ -241,7 +241,7 @@ public class RegularPlay extends AppCompatActivity implements GoogleApiClient.Co
             case KeyEvent.KEYCODE_BACK: endGame(); break;
             case KeyEvent.KEYCODE_HOME: endGame(); break;
             case KeyEvent.KEYCODE_MENU: endGame(); break;
-            default: endGame(); break;
+            case KeyEvent.KEYCODE_POWER: endGame(); break;
         }
         return super.onKeyDown(keyCode, event);
     }
